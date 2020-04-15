c53e216a32b7c911d82289c4bed55a3356379ebf
Amaury Martiny <amaury.martiny@protonmail.com>
2020-04-15 11:31:33 +0200

    Make http work

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,31 @@
 // You should have received a copy of the GNU General Public License
 // along with Reacher.  If not, see <http://www.gnu.org/licenses/>.
 
-mod http;
+mod handlers;
 
 use std::env;
+use std::net::SocketAddr;
+use warp::Filter;
 
+/// Run a HTTP server using warp.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
+
+    let cors = warp::cors()
+        .allow_origins(vec!["http://127.0.0.1:3000", "https://reacherhq.github.io"])
+        .allow_headers(vec!["*"])
+        .allow_methods(vec!["POST"]);
+
+    // POST / {"to_email":""}
+    let routes = warp::post()
+        .and(warp::path("check_email"))
+        // When accepting a body, we want a JSON body (and to reject huge
+        // payloads)...
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::json())
+        .and_then(handlers::check_email)
+        .with(cors);
 
     // Since we're running the HTTP server inside a Docker container, we
     // use 0.0.0.0. Allow for overriding via env variable.
@@ -30,8 +48,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // - if not set, then the $PORT env varialbe
     // - if not set, then 8080
     let http_port = env::var("PORT").unwrap_or_else(|_| "8080".into());
+    let addr = SocketAddr::new(http_host.parse()?, http_port.parse()?);
 
-    http::run(&http_host, http_port.parse::<u16>().unwrap()).await?;
-
-    Ok(())
+    Ok(warp::serve(routes).run(addr).await)
 }
