fa034b01c843f42fbb1573a0dcea34606d1e0f9e
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-16 16:44:25 +0200

    Temporarily remove saasify check

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,7 @@
 // along with Reacher.  If not, see <http://www.gnu.org/licenses/>.
 
 mod handlers;
-mod saasify_secret;
 
-use saasify_secret::check_saasify_secret;
 use std::{env, net::IpAddr};
 use warp::Filter;
 
@@ -26,10 +24,6 @@ fn create_api() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejecti
 	// POST /check_email
 	warp::path("check_email")
 		.and(warp::post())
-		// FIXME We should be able to just use warp::header::exact, and remove
-		// completely `./saasify_secret.rs`.
-		// https://github.com/seanmonstar/warp/issues/503
-		.and(check_saasify_secret())
 		// When accepting a body, we want a JSON body (and to reject huge
 		// payloads)...
 		.and(warp::body::content_length_limit(1024 * 16))
@@ -85,45 +79,13 @@ mod tests {
 	use warp::http::StatusCode;
 	use warp::test::request;
 
-	use super::{create_api, handlers::EmailInput, saasify_secret::SAASIFY_SECRET_HEADER};
-
-	#[tokio::test]
-	async fn test_missing_saasify_secret() {
-		let resp = request()
-			.path("/check_email")
-			.method("POST")
-			.reply(&create_api())
-			.await;
-
-		assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
-		assert_eq!(
-			resp.body(),
-			"Missing request header \"x-saasify-proxy-secret\"".as_bytes()
-		);
-	}
-
-	#[tokio::test]
-	async fn test_incorrect_saasify_secret() {
-		let resp = request()
-			.path("/check_email")
-			.method("POST")
-			.header(SAASIFY_SECRET_HEADER, "incorrect")
-			.reply(&create_api())
-			.await;
-
-		assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
-		assert_eq!(
-			resp.body(),
-			"Unhandled rejection: IncorrectSaasifySecret".as_bytes()
-		);
-	}
+	use super::{create_api, handlers::EmailInput};
 
 	#[tokio::test]
 	async fn test_input_foo_bar() {
 		let resp = request()
 			.path("/check_email")
 			.method("POST")
-			.header(SAASIFY_SECRET_HEADER, "reacher_dev_secret")
 			.json(&serde_json::from_str::<EmailInput>(r#"{"to_email": "foo@bar"}"#).unwrap())
 			.reply(&create_api())
 			.await;
@@ -140,7 +102,6 @@ mod tests {
 		let resp = request()
 			.path("/check_email")
 			.method("POST")
-			.header(SAASIFY_SECRET_HEADER, "reacher_dev_secret")
 			.json(&serde_json::from_str::<EmailInput>(r#"{"to_email": "foo@bar.baz"}"#).unwrap())
 			.reply(&create_api())
 			.await;
