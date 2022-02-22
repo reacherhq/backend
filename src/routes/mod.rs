f49d1ef9fc42fb9ea3aa6dca32630a5de4c887cf
Amaury <1293565+amaurym@users.noreply.github.com>
2022-02-22 19:12:02 +0100

    refactor: Rename manage_job to bulk (#277)
    * refactor: Rename manage_job to bulk
    
    * fix lint

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+pub mod bulk;
 pub mod check_email;
-pub mod manage_job;
 pub mod version;
 
 use super::errors;
@@ -27,10 +27,8 @@ pub fn create_routes(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
 	version::get::get_version()
 		.or(check_email::post::post_check_email())
-		.or(manage_job::post::create_bulk_email_vrfy_job(
-			conn_pool.clone(),
-		))
-		.or(manage_job::get::get_job_status(conn_pool.clone()))
-		.or(manage_job::get::get_job_result(conn_pool))
+		.or(bulk::post::create_bulk_email_vrfy_job(conn_pool.clone()))
+		.or(bulk::get::get_job_status(conn_pool.clone()))
+		.or(bulk::get::get_job_result(conn_pool))
 		.recover(errors::handle_rejection)
 }
