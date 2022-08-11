64e345f6f76566b8b2250ca8a58b940459d09cb1
Amaury <1293565+amaurym@users.noreply.github.com>
2022-08-11 18:26:25 +0200

    refactor: Put bulk endpoints behind a feature flag (#320)
    * Add bulk behind feature flag
    
    * Add features in ci
    
    * Fix ci
    
    * Fix tests

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -14,14 +14,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#[cfg(feature = "bulk")]
 pub mod bulk;
 pub mod check_email;
 pub mod version;
 
 use super::errors;
+#[cfg(feature = "bulk")]
 use sqlx::{Pool, Postgres};
 use warp::Filter;
 
+#[cfg(feature = "bulk")]
 pub fn create_routes(
 	conn_pool: Pool<Postgres>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -30,5 +33,12 @@ pub fn create_routes(
 		.or(bulk::post::create_bulk_job(conn_pool.clone()))
 		.or(bulk::get::get_bulk_job_status(conn_pool.clone()))
 		.or(bulk::download::get_bulk_job_result(conn_pool))
+		.recover(errors::handle_rejection)
+}
+
+#[cfg(not(feature = "bulk"))]
+pub fn create_routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+	version::get::get_version()
+		.or(check_email::post::post_check_email())
 		.recover(errors::handle_rejection)
 }
